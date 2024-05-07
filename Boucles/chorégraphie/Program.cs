daccfd7c67763d706ec6a879f9091f29938a53d5
NatVCM <147696519+NatVCM@users.noreply.github.com>
2024-05-07 11:00:34 +0200

    Add files via upload

---
+++ Boucles/chorégraphie/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace chorégraphie;
+
+class Program
+{
+   static void Main(string[] args)
+    {
+        string[] moves =new string [32];
+
+        int index = 0;
+
+        bool done = false;
+
+
+        while (!done)
+        {
+
+            Console.WriteLine("Entrez un pas de danse:");
+            string saisieUtilisateur = Console.ReadLine();
+
+            if (saisieUtilisateur == "done")
+            {
+                done = true;
+            }
+
+            else
+            {
+
+                if (index ==31)
+                {
+                    done = true;
+                }
+                else
+                {
+
+
+                    moves[index] = saisieUtilisateur;
+
+                    index++;
+
+
+                    Console.WriteLine("You " + moves[index]);
+
+                }
+
+               
+                
+
+            }
+
+          
+
+        }
+
+        Console.WriteLine("You finished your danse!!");
+
+       
+
+
+
+
+    }
+}
+
